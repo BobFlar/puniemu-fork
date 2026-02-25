abcfa52c1cb7a16418b2a623b09d1e01c928b15c
Nohan <nohanperino1@gmail.com>
2026-02-25 22:09:45 +0100

    Update GenerateFriendData.cs

--- Src/Server/GameServer/Logic/GenerateFriendData.cs
+++ Src/Server/GameServer/Logic/GenerateFriendData.cs
@@ -83,6 +83,7 @@ namespace Puniemu.Src.Server.GameServer.Logic
                             item2.YoukaiId = YoukaiId;
                         if (!LastPlayDt.IsNullOrEmpty())
                             item2.LastPlayDt = LastPlayDt;
+                        item2.LastPlayDtSentence = GameServer.Logic.GenerateFriendData.GetTimeDifferenceString(LastPlayDt);
                     }
                 }
                 await UserDataManager.Logic.UserDataManager.SetYwpUserAsync(targetGdkey, "ywp_user_friend", friendFriendList!);
