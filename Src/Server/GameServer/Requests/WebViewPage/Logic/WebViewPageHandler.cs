dd017ed8d7f8e9a5610b6dcbfcc7c93a7dbbe16a
Nohan <nohanperino1@gmail.com>
2026-02-26 15:44:43 +0100

    Create WebViewPageHandler.cs

---
+++ Src/Server/GameServer/Requests/WebViewPage/Logic/WebViewPageHandler.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Puniemu.Src.Server.GameServer.Requests.WebViewPage.Logic
+{
+    public static class WebViewPageHandler
+    {
+        public static async Task HandleAsync(HttpContext ctx)
+        {
+            string webviewId = ctx.Request.Query["webviewId"].ToString();
+
+            if (string.IsNullOrEmpty(webviewId))
+            {
+                ctx.Response.StatusCode = 200;
+                await ctx.Response.WriteAsync("webviewId is empty");
+                return;
+            }
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Web", "Page", webviewId);
+            string filePath = Path.Combine(folderPath, "viewpage.html");
+
+            ctx.Response.ContentType = "text/html; charset=utf-8";
+
+            if (!Directory.Exists(folderPath))
+            {
+                ctx.Response.StatusCode = 200;
+                await ctx.Response.WriteAsync("This page doesn't exist");
+                return;
+            }
+            
+            if (File.Exists(filePath))
+            {
+                await ctx.Response.SendFileAsync(filePath);
+            }
+            else
+            {
+                ctx.Response.StatusCode = 200;
+                await ctx.Response.WriteAsync("This page was not found, please check that the file name is \"viewpage.html\"");
+            }
+        }
+    }
+}
